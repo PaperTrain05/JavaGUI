fba6109d1a990791832fcf21d83399d486e91cd4
PaperTrain05 <michelemameli5@gmail.com>
2024-04-16 20:09:55 +0200

    Inziato il Form

---
+++ src/main/java/it/paper/MainGUI.java
@@ -0,0 +1,15 @@
+package it.paper;
+
+import javax.swing.*;
+
+public class MainGUI extends JFrame {
+
+    private JTextField textField1;
+
+    public MainGUI() {
+        setSize(100, 100);
+        setVisible(true);
+        setLocationRelativeTo(null);
+
+    }
+}
